2f53e9f9bbd2e3323c5e8c40dfba448989857875
xavier G <xavier457@hotmail.com>
2015-03-23 11:30:18 -0430

    shema default false task

--- db/migrate/20150323151052_create_tasks.rb
+++ db/migrate/20150323151052_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.string :task
-      t.boolean :status
+      t.boolean :status, default: false
 
       t.timestamps null: false
     end
